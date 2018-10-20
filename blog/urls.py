d04ded8845bc8de3d3bc0f6559b425fb646620e8
kauziishere <kaustubhjoshi1411@gmail.com>
2018-10-20 19:04:56 +0530

    Added my blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.post_list, name = 'post_list')
+    path('', views.index_page, name = 'index'),
+    path(r'edu',views.edu_page, name = 'edu'),
+    path(r'skills',views.skills_page, name = 'skills'),
+    path(r'courses',views.courses_page, name = 'courses'),
+    path(r'projects',views.projects_page, name = 'projects'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
-
+from .models import Post, Skills
+from django.utils import timezone
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.all()
+	return render(request, 'blog/post_list.html', {})
+def index_page(request):
+	posts = Post.objects.get(title='About me')
+	posts.view_count = posts.view_count + 1
+	posts.save()
+	return render(request, 'blog/index.html', {'posts': posts})
+
+def edu_page(request):
+	posts = Post.objects.get(title='Education')
+	posts.view_count = posts.view_count + 1
+	posts.save()
+	return render(request, 'blog/edu.html', {'posts': posts})
+def courses_page(request):
+	posts = Post.objects.get(title='Courses')
+	posts.view_count = posts.view_count + 1
+	posts.save()
+	return render(request, 'blog/courses.html', {'posts': posts})
+def projects_page(request):
+	posts = Post.objects.get(title='Projects')
+	posts.view_count = posts.view_count + 1
+	posts.save()
+	return render(request, 'blog/projects.html', {'posts': posts})
+def skills_page(request):
+	posts = Post.objects.get(title='Skills')
+	posts.view_count = posts.view_count + 1
+	posts.save()
+	return render(request, 'blog/skills.html', {'posts': posts})
